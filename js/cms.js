1c7d5b8d35bb8c3ebd3025de10ac2d8341dd52d7
Eliad Saporta <eliad@sapdan.com>
2016-10-05 11:45:05 +0300

    removing title

--- js/cms.js
+++ js/cms.js
@@ -30,7 +30,7 @@ var CMS = {
 		loader: '<div class="loader">Loading...</div>',
 		get siteAttributes() {
 			return [
-				{ attr: 'title', value: CMS.settings.siteName },
+				
 				{ attr: '.cms_sitename', value: CMS.settings.siteName},
 				{ attr: '.cms_tagline', value: CMS.settings.siteTagline},
 				{ attr: '.cms_footer_text', value: CMS.settings.footerText}
